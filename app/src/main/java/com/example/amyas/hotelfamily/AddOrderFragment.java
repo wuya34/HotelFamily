a1dc84fb0347f0d9583ef8b8751379ddda9b6dbd
Amyas <geniusafteramyas@163.com>
2017-11-20 17:28:01 +0800

    添加AddOrderFragment滚轮选择

--- app/src/main/java/com/example/amyas/hotelfamily/AddOrderFragment.java
+++ app/src/main/java/com/example/amyas/hotelfamily/AddOrderFragment.java
@@ -1,15 +1,20 @@
 package com.example.amyas.hotelfamily;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
+import com.example.amyas.hotelfamily.db.OrderDbSchema;
 import com.example.amyas.hotelfamily.model.Order;
 import com.example.amyas.hotelfamily.model.OrderLab;
-import com.example.amyas.hotelfamily.util.ToastUtil;
+import com.example.amyas.hotelfamily.util.PickerFragment;
 
 import java.util.UUID;
 
@@ -18,15 +23,22 @@ import java.util.UUID;
  * date: 2017/11/18
  */
 
-public class AddOrderFragment extends Fragment {
+public class AddOrderFragment extends Fragment implements View.OnClickListener {
     public static final int UPDATE_ORDER_CODE = 1;
     public static final int ADD_ORDER_CODE = 2;
-    private Order mOrder;
+    public static final int DESK_PICK = 3;
+    public static final int NUMBER_PICK = 4;
+    public static final int REQUEST_CODE = 5;
+    public static final int RESULT_OK = 6;
+    public static final String DIALOG_PICKER = "DialogPicker";
     public static final String ORDER_UUID = "AddOrderFragment.uuid";
+    private Order mOrder;
+    private Button mSelectDesk;
+    private Button mSelectNumber;
 
-    public static AddOrderFragment newInstance(UUID uuid){
+    public static AddOrderFragment newInstance(UUID uuid) {
         AddOrderFragment fragment = new AddOrderFragment();
-        if (uuid!=null){
+        if (uuid != null) {
             Bundle args = new Bundle();
             args.putSerializable(ORDER_UUID, uuid);
             fragment.setArguments(args);
@@ -42,15 +54,62 @@ public class AddOrderFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.add_order,container, false);
+        View view = inflater.inflate(R.layout.add_order, container, false);
+
         Bundle bundle = this.getArguments();
-        if (bundle!=null){
+        if (bundle != null) {
             UUID uuid = (UUID) bundle.getSerializable(ORDER_UUID);
-//            ToastUtil.showToast(getActivity(), "received uuid: "+uuid);
+            //            ToastUtil.showToast(getActivity(), "received uuid: "+uuid);
             mOrder = OrderLab.get(getActivity()).getOrder(uuid);
         }
 
+        mSelectDesk = view.findViewById(R.id.select_desk);
+        mSelectNumber = view.findViewById(R.id.select_number);
+        mSelectNumber.setOnClickListener(this);
+        mSelectDesk.setOnClickListener(this);
         // TODO: 2017/11/18 增加菜单选项
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.select_number:
+                prepareLoopView("用餐人数：", NUMBER_PICK);
+                break;
+            case R.id.select_desk:
+                prepareLoopView("可选桌号", DESK_PICK);
+                break;
+        }
+    }
+
+    private void prepareLoopView(String title, int type) {
+        PickerFragment dialog = PickerFragment.newInstance(title, type);
+        FragmentManager manager = getFragmentManager();
+        dialog.setTargetFragment(AddOrderFragment.this, REQUEST_CODE);
+        dialog.show(manager, DIALOG_PICKER);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_CODE) {
+            int type = (int) data.getSerializableExtra(PickerFragment.EXTRA_TYPE);
+            String text = (String) data.getSerializableExtra(PickerFragment.EXTRA_DATA);
+            if (type==NUMBER_PICK){
+                mSelectNumber.setText(text);
+            }
+            if (type==DESK_PICK){
+                mSelectDesk.setText(text);
+            }
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        OrderLab.get(getActivity()).updateOrder(mOrder);
     }
 }
---
+++ app/src/main/java/com/example/amyas/hotelfamily/util/PickerFragment.java
@@ -0,0 +1,105 @@
+package com.example.amyas.hotelfamily.util;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.example.amyas.hotelfamily.AddOrderFragment;
+import com.example.amyas.hotelfamily.R;
+import com.weigan.loopview.LoopView;
+import com.weigan.loopview.OnItemSelectedListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * author: Amyas
+ * date: 2017/11/20
+ */
+
+public class PickerFragment extends DialogFragment {
+    public static final String EXTRA_DATA = "pickerFragment.data";
+    public static final String EXTRA_TYPE = "pickerFragment.type";
+    private List<String> mList;
+    private String mTitle;
+    private LoopView mLoopView;
+    private int mType;
+    private int expectIndex;
+    private int currentIndex;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_picker,null);
+        inflateData();
+        FrameLayout frameLayout = view.findViewById(R.id.picker_container);
+        mLoopView = new LoopView(getActivity());
+        mLoopView.setNotLoop();
+        mLoopView.setListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int index) {
+                currentIndex = index;
+                ToastUtil.showToast(getActivity(), "index is"+index+"选择了"+ mList.get(currentIndex));
+            }
+        });
+        mLoopView.setItems(mList);
+        mLoopView.setInitPosition(expectIndex);
+        mLoopView.setTextSize(30);
+        frameLayout.addView(mLoopView);
+        return new AlertDialog.Builder(getActivity())
+                .setView(view)
+                .setTitle(mTitle)
+                .setPositiveButton(R.string.done, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        sendResult(AddOrderFragment.RESULT_OK);
+                    }
+                })
+                .create();
+    }
+    public static PickerFragment newInstance(String title, int type){
+        PickerFragment fragment = new PickerFragment();
+        fragment.mTitle = title;
+        fragment.mType = type;
+        return fragment;
+    }
+    private void inflateData(){
+        mList = new ArrayList<>();
+        if (mType== AddOrderFragment.NUMBER_PICK){
+
+            for (int i = 1; i < 21; i++) {
+                mList.add(i+" 人");
+            }
+            expectIndex = 5;
+            return;
+        }
+        if (mType==AddOrderFragment.DESK_PICK){
+            mList.add("6");
+            mList.add("8");
+            mList.add("66");
+            mList.add("88");
+            mList.add("666");
+            mList.add("888");
+            expectIndex = 0;
+        }
+    }
+    private void sendResult(int resultCode){
+        if (getTargetFragment()==null){
+            return;
+        }
+        String result = mList.get(currentIndex);
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_DATA, result);
+        intent.putExtra(EXTRA_TYPE, mType);
+
+        getTargetFragment().onActivityResult(getTargetRequestCode(),resultCode, intent);
+    }
+}
